e9f4ef7035c2b9a3feeb64b95b00c58fd09a0de0
EmilianoQuiroz <santiagoquiroz.dev@gmail.com>
2025-12-16 10:48:00 +0000

    feat: section list and pull to refresh

--- app/pull-to-refresh/index.tsx
+++ app/pull-to-refresh/index.tsx
@@ -1,10 +1,30 @@
-import { View, Text } from 'react-native';
+import ThemeText from "@/presentation/shared/ThemedText";
+import ThemedView from "@/presentation/shared/ThemedView";
+import { useState } from "react";
+import { RefreshControl, ScrollView } from "react-native";
 
 const PullToRefreshScreen = () => {
+  const [isRefreshing, setIsRefreshing] = useState(false);
+
   return (
-    <View>
-      <Text>PullToRefreshScreen</Text>
-    </View>
+    <ScrollView
+      refreshControl={
+        <RefreshControl
+          refreshing={isRefreshing}
+          onRefresh={() => {
+            setIsRefreshing(true);
+            setTimeout(() => {
+              setIsRefreshing(false);
+            }, 2000);
+          }}
+          colors={["#9Bd35A", "#689F38"]}
+        />
+      }
+    >
+      <ThemedView margin>
+        <ThemeText>PullToRefreshScreen</ThemeText>
+      </ThemedView>
+    </ScrollView>
   );
 };
 export default PullToRefreshScreen;
--- app/section-list/index.tsx
+++ app/section-list/index.tsx
@@ -1,10 +1,45 @@
-import { View, Text } from 'react-native';
+import ThemedView from "@/presentation/shared/ThemedView";
+import { View, Text, SectionList } from "react-native";
+import { houses } from "./section-list-data";
+import ThemeText from "@/presentation/shared/ThemedText";
 
 const SectionListScreen = () => {
   return (
-    <View>
-      <Text>SectionListScreen</Text>
-    </View>
+    <ThemedView margin>
+      <SectionList
+        sections={houses}
+        keyExtractor={(item, index) => item + index}
+        renderItem={({ item }) => <ThemeText>{item}</ThemeText>}
+        ListHeaderComponent={() => (
+          <ThemeText
+            style={{ fontSize: 24, fontWeight: "bold", marginBottom: 10 }}
+          >
+            Section List Example
+          </ThemeText>
+        )}
+        renderSectionHeader={({ section: { title } }) => (
+          <ThemeText
+            style={{
+              fontSize: 20,
+              fontWeight: "600",
+              backgroundColor: "#eee",
+              padding: 5,
+              marginTop: 10,
+            }}
+          >
+            {title}
+          </ThemeText>
+        )}
+        stickySectionHeadersEnabled={true}
+        ListFooterComponent={() => (
+          <ThemeText
+            style={{ fontSize: 16, fontWeight: "bold", marginTop: 20 }}
+          >
+            End of Section List
+          </ThemeText>
+        )}
+      />
+    </ThemedView>
   );
 };
 export default SectionListScreen;
